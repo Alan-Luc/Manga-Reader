f46ad7098ff8ccc8b08576106a9da1b668d4a8ba
WilliamJYi <yiwilliam1098@gmail.com>
2021-05-15 09:52:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () =>{
-
     const [pages, setPages] = useState();
+    const [toggle, setToggle] = useState(false)
 
     //const accessToken = "Bearer 6b478c21d23e90b19dbcaf8523c28659d422716b"
 
     const handleClick = () =>{
-        fetch("https://api.imgur.com/3/album/zYLDaPN", {
+      fetch("https://api.imgur.com/3/album/zYLDaPN", {
             method: "GET",
             headers: {
                 Authorization:"Client-ID 7ce042b065faaa3"
@@ -19,21 +20,19 @@ const App = () =>{
             console.log(data);
             setPages(data.data.images);
             console.log(pages)
-        });
+      });
     }
 
     const displayPages = () =>{
-      return(
-        <div>
-          {pages !== null ? pages.map(image => <img src={image.link} alt="manga page"/>) : console.log("no pages")};
-        </div>
-      )
+      setToggle(true);
+      console.log(pages)
     }
 
     return(
         <div>
             <button onClick={handleClick}>click</button>
             <button onClick={displayPages}>get pages</button>
+            {toggle ? pages.map(item => <img src={item.link} alt="manga page" key={uuidv4()} width="200"/>) : null}
         </div>
     )
 }
