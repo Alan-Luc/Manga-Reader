0afac16e90e384f58a61920b5562dba872486431
WilliamJYi <yiwilliam1098@gmail.com>
2021-05-15 10:30:20 -0400

    initial push

--- src/App.js
+++ src/App.js
@@ -9,20 +9,22 @@ const App = () =>{
 
     //const accessToken = "Bearer 6b478c21d23e90b19dbcaf8523c28659d422716b"
 
-    /*const handleClick = () =>{
-      fetch("https://api.imgur.com/3/album/zYLDaPN", {
-            method: "GET",
-            headers: {
-                Authorization:"Client-ID 7ce042b065faaa3"
-            }
-        })
-        .then(data => data.json())
-        .then(data => {
-            console.log(data);
-            setPages(data.data.images);
-            console.log(pages)
-      });
-    }*/
+    const getPages = () =>{
+      if(query !== ""){
+        fetch(`https://api.imgur.com/3/album/${query}`, {
+              method: "GET",
+              headers: {
+                  Authorization:"Client-ID 7ce042b065faaa3"
+              }
+          })
+          .then(data => data.json())
+          .then(data => {
+              console.log(data);
+              setPages(data.data.images);
+              console.log(pages)
+        });
+      }
+    }
 
     const handleChange = e =>{
       setQuery(e.target.value);
@@ -30,19 +32,11 @@ const App = () =>{
 
     const handleSubmit = e =>{
       e.preventDefault();
-      fetch(`https://api.imgur.com/3/album/${query}`, {
-            method: "GET",
-            headers: {
-                Authorization:"Client-ID 7ce042b065faaa3"
-            }
-        })
-        .then(data => data.json())
-        .then(data => {
-            console.log(data);
-            setPages(data.data.images);
-            console.log(pages)
-      });
-      setToggle(true);
+      getPages();
+      if(pages != null){
+        setToggle(true);
+      }
+      
     }
 
     return(
