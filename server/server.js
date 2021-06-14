5aadcdee224de6785060b6cedd1ecf03168dfe2c
WilliamJYi <yiwilliam1098@gmail.com>
2021-06-14 14:38:07 -0400

    proxy testing

--- server/server.js
+++ server/server.js
@@ -6,13 +6,20 @@ const path = require("path");
 
 const PORT = process.env.PORT || 8000;
 
+const corsOptions = {
+    origin: "https://simplemangareader.netlify.app/",
+    methods: ["GET"],
+    allowedHeaders: ["Access-Control-Allow-Origin"],
+    credentials: true,
+}
+
 const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, './build')));
 
-app.use(cors());
+app.use(cors(corsOptions));
 
-app.get("*", (req, res) => {
+app.get("/*", cors(corsOptions),(req, res) => {
     res.sendFile(path.join(__dirname + "./build/index.html"));
 }); //used in production, comment out if wanting to test
 
