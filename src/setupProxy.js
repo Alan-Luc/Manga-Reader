dbd76875c6edf58717a1cfad6b23f17f8ddc892d
WilliamJYi <yiwilliam1098@gmail.com>
2021-06-14 13:48:55 -0400

    proxy testing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,15 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
     app.use(
-        "/manga",
-        createProxyMiddleware({
-            target: "https://api.mangadex.org",
-            changeOrigin: true,
-        })
-    );
-
-    app.use(
-        "/chapter",
+        "/",
         createProxyMiddleware({
             target: "https://api.mangadex.org",
             changeOrigin: true,
